1686f00dcd77f2fce31cd3b102a4f13941595513
Sagar Ganesh <84278200+sagarganesh8@users.noreply.github.com>
2022-08-13 21:26:15 +0530

    Update emailTemplate.js
    Added Email Link

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -148,7 +148,7 @@ module.exports = ({emailFrom, downloadLink, size, expiresIn}) => {
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                                 <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">FileTransfer India </span>
-                                <br> Want to share a file? <a href="https://filetransferapp.herokuapp.com/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">FileTransferWebApplication</a>.
+                                <br> Want to share a file? <a href="https://filetransferwebapp.herokuapp.com/"https://filetransferwebapp.herokuapp.com/ style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">FileTransferWebApplication</a>.
                             </td
                             </tr>
                             <tr>
